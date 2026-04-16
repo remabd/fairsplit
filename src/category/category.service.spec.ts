f704f416766125806c54feb129174539a7f6e5b2
remabd <remiabdallah3@gmail.com>
2026-04-16 18:34:46 +0200

    feat-format: format

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { Category } from 'src/entities';
 
 describe('CategoryService', () => {
-  let service: CategoryService;
+    let service: CategoryService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [CategoryService],
+        }).compile();
 
-    service = module.get<CategoryService>(CategoryService);
-  });
+        service = module.get<CategoryService>(CategoryService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe("Find categories", ()=> {
+        it('Should return all categories', ()=> {
+            const categories = service.findAll();
+            expect(categories).toBeInstanceOf(Category[]);
+        })
+    })
 });
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -4,23 +4,23 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+    create(createCategoryDto: CreateCategoryDto) {
+        return 'This action adds a new category';
+    }
 
-  findAll() {
-    return `This action returns all category`;
-  }
+    findAll() {
+        return `This action returns all category`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} category`;
+    }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
+    update(id: number, updateCategoryDto: UpdateCategoryDto) {
+        return `This action updates a #${id} category`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} category`;
+    }
 }
